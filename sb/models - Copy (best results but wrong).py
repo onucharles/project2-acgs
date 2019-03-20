Fixing gradient
Tried some fixes for poor ppl

sb/models - Copy (best results but wrong).py
@@ -293,42 +293,48 @@ class GRU(nn.Module): # Implement a stacked GRU RNN
         """
         device = torch.device("cuda")
 
-        #x = torch.zeros([self.seq_len, self.batch_size, self.emb_size]).to(device)
-        logits_list = []#torch.zeros([self.seq_len, self.batch_size, self.vocab_size]).to(device)
-        hidden_seq_list = []
-        #r = torch.zeros([self.seq_len, self.num_layers, self.batch_size, self.hidden_size]).to(device)
-        #z = torch.zeros([self.seq_len, self.num_layers, self.batch_size, self.hidden_size]).to(device)
-        #h_tilde = torch.zeros([self.seq_len, self.num_layers, self.batch_size, self.hidden_size]).to(device)
+        x = torch.zeros([self.seq_len, self.batch_size, self.emb_size]).to(device)
+        logits = torch.zeros([self.seq_len, self.batch_size, self.vocab_size]).to(device)
+        
+        r = torch.zeros([self.seq_len, self.num_layers, self.batch_size, self.hidden_size]).to(device)
+        z = torch.zeros([self.seq_len, self.num_layers, self.batch_size, self.hidden_size]).to(device)
+        h_tilde = torch.zeros([self.seq_len, self.num_layers, self.batch_size, self.hidden_size]).to(device)
+        hidden_seq = torch.zeros([self.seq_len, self.num_layers, self.batch_size, self.hidden_size]).to(device)
 
-        hidden_seq_list.append(hidden)
+        hidden_seq[0] = hidden
 
         for t in range(self.seq_len):
-            hidden_layer_list = []
-
             #Embedding
-            x = self.embedding(inputs[t])
-            x = self.dropout(x)
-            
+            x[t] = self.embedding(inputs[t])
+            x[t] = self.dropout(x[t])
+
             for layer in range(self.num_layers):
                 #For the first layer, we use the input
                 if layer == 0:
-                    r = self.activation_r(self.lin_Wr[layer](x) + self.lin_Ur[layer](hidden_seq_list[t][layer]))
-                    z = self.activation_z(self.lin_Wz[layer](x) + self.lin_Uz[layer](hidden_seq_list[t][layer]))
-                    h_tilde = self.activation_h(self.lin_Wh[layer](x) + self.lin_Uh[layer](r*hidden_seq_list[t][layer]))
+                    r[t, layer] = self.lin_Wr[layer](x[t]) + self.lin_Ur[layer](hidden_seq[t, layer])
+                    r[t, layer] = self.activation_r(r[t, layer])
+
+                    z[t, layer] = self.lin_Wz[layer](x[t]) + self.lin_Uz[layer](hidden_seq[t, layer])
+                    z[t, layer] = self.activation_z(z[t, layer])
+
+                    h_tilde[t, layer] = self.lin_Wh[layer](x[t]) + self.lin_Uh[layer](r[t, layer]*hidden_seq[t, layer])
+                    h_tilde[t, layer] = self.activation_h(h_tilde[t, layer])
 
                 #For the other layers, we use the previous layer instead of the input
                 else:
-                    r = self.activation_r(self.lin_Wr[layer](hidden_layer_list[layer-1]) + self.lin_Ur[layer](hidden_seq_list[t][layer]))
-                    z = self.activation_z(self.lin_Wz[layer](hidden_layer_list[layer-1]) + self.lin_Uz[layer](hidden_seq_list[t][layer]))
-                    h_tilde = self.activation_h(self.lin_Wh[layer](hidden_layer_list[layer-1]) + self.lin_Uh[layer](r*hidden_seq_list[t][layer]))
+                    r[t, layer] = self.lin_Wr[layer](hidden[layer-1]) + self.lin_Ur[layer](hidden_seq[t, layer])
+                    r[t, layer] = self.activation_r(r[t, layer])
 
-                hidden_layer_list.append(self.dropout((1-z)*hidden_seq_list[t][layer] + z*h_tilde))
+                    z[t, layer] = self.lin_Wz[layer](hidden[layer-1]) + self.lin_Uz[layer](hidden_seq[t, layer])
+                    z[t, layer] = self.activation_z(z[t, layer])
+
+                    h_tilde[t, layer] = self.lin_Wh[layer](hidden[layer-1]) + self.lin_Uh[layer](r[t, layer]*hidden_seq[t, layer])
+                    h_tilde[t, layer] = self.activation_h(h_tilde[t, layer])
+
+                hidden_seq[t, layer] = (1-z[t, layer])*hidden_seq[t, layer] + z[t, layer]*h_tilde[t, layer]
+                hidden_seq[t, layer] = self.dropout(hidden_seq[t, layer])
             
-            hidden_seq_list.append(torch.stack(hidden_layer_list))
-            logits_list.append(self.lin_Wy(hidden_seq_list[t+1][self.num_layers-1]))
-        
-        logits = torch.stack(logits_list)
-        hidden = hidden_seq_list[self.seq_len]
+            logits[t] = self.lin_Wy(hidden[self.num_layers-1])     
 
         return logits.view(self.seq_len, self.batch_size, self.vocab_size), hidden
 

sb/ptb-lm.py
@@ -77,6 +77,7 @@
 #      GRU.  Implementing this method is not considered part of problems 1/2 
 #      respectively, and will be graded as part of Problem 5.3
 
+
 import argparse
 import time
 import collections
@@ -91,8 +92,8 @@ np = numpy
 
 # NOTE ==============================================
 # This is where your models are imported
-from models import GRU#, RNN 
-from models import make_model as TRANSFORMER
+from models import GRU#RNN, GRU 
+#from models import make_model as TRANSFORMER
 
 
 ##############################################################################
@@ -161,8 +162,8 @@ argsdict['code_file'] = sys.argv[0]
 print("\n########## Setting Up Experiment ######################")
 flags = [flag.lstrip('--').replace('/', '').replace('\\', '') for flag in sys.argv[1:]]
 experiment_path = os.path.join(args.save_dir+'_'.join([argsdict['model'],
-                                        argsdict['optimizer']] 
-                                        + flags))
+                                         argsdict['optimizer']] 
+                                         + flags))
 
 # Increment a counter so that previous results with the same args will not
 # be overwritten. Comment out the next four lines if you only want to keep
@@ -189,21 +190,20 @@ if torch.cuda.is_available():
     device = torch.device("cuda") 
 else:
     print("WARNING: You are about to run on cpu, and this will likely run out \
-    of memory. \n You can try setting batch_size=1 to reduce memory usage")
+      of memory. \n You can try setting batch_size=1 to reduce memory usage")
     device = torch.device("cpu")
 
 
 ###############################################################################
 #
-# 
-#LOADING & PROCESSING
+# LOADING & PROCESSING
 #
 ###############################################################################
 
 # HELPER FUNCTIONS
 def _read_words(filename):
     with open(filename, "r") as f:
-        return f.read().replace("\n", "<eos>").split()
+      return f.read().replace("\n", "<eos>").split()
 
 def _build_vocab(filename):
     data = _read_words(filename)
@@ -320,7 +320,7 @@ elif args.model == 'TRANSFORMER':
     model.seq_len=args.seq_len
     model.vocab_size=vocab_size
 else:
-    print("Model type not recognized.")
+  print("Model type not recognized.")
 
 model = model.to(device)
 
@@ -354,7 +354,7 @@ def repackage_hidden(h):
     This is the case with the way we've processed the Penn Treebank dataset.
     """
     if isinstance(h, Variable):
-        return h.detach_()
+        return h.detach()
     else:
         return tuple(repackage_hidden(v) for v in h)
 
@@ -489,9 +489,9 @@ for epoch in range(num_epochs):
 lc_path = os.path.join(args.save_dir, 'learning_curves.npy')
 print('\nDONE\n\nSaving learning curves to '+lc_path)
 np.save(lc_path, {'train_ppls':train_ppls, 
-                'val_ppls':val_ppls, 
-                'train_losses':train_losses,
-                'val_losses':val_losses})
+                  'val_ppls':val_ppls, 
+                  'train_losses':train_losses,
+                  'val_losses':val_losses})
 # NOTE ==============================================
 # To load these, run 
 # >>> x = np.load(lc_path)[()]