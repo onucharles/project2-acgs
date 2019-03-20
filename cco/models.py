Fixed issue with softmax dimension in transformer.

cco/models.py
@@ -296,10 +296,10 @@ class MultiHeadedAttention(nn.Module):
         # perform scaled-dot product attention
         A_presoftmax = torch.matmul(query, key.transpose(2,3)) / math.sqrt(self.d_k) # dim (batch_size, n_heads, seq_len, seq_len)
         if not mask is None:
-            mask = torch.unsqueeze(mask, 1) # add dimension of 1 at loc of n_heads to allow broadcasting
+            mask = mask.unsqueeze(1) # add dimension of 1 at loc of n_heads to allow broadcasting
             A_presoftmax = A_presoftmax.masked_fill(mask == 0, -10**9)
 
-        A = F.softmax(A_presoftmax, dim=1)  # apply softmax on n_heads
+        A = F.softmax(A_presoftmax, dim=-1)  # apply softmax on n_heads
         if self.dropout:
             A = self.dropout(A)
 
@@ -312,75 +312,6 @@ class MultiHeadedAttention(nn.Module):
         attention_values = self.output_linear(H_concat)
 
         return attention_values # size: (batch_size, seq_len, self.n_units)
-
-# # TODO: implement this class
-# class MultiHeadedAttention(nn.Module):
-#     def __init__(self, n_heads, n_units, dropout=0.1):
-#         """
-#         n_heads: the number of attention heads
-#         n_units: the number of output units
-#         dropout: probability of DROPPING units
-#         """
-#         super(MultiHeadedAttention, self).__init__()
-#         # This sets the size of the keys, values, and queries (self.d_k) to all
-#         # be equal to the number of output units divided by the number of heads.
-#         self.d_k = n_units // n_heads
-#         # This requires the number of n_heads to evenly divide n_units.
-#         assert n_units % n_heads == 0
-#         self.n_units = n_units
-#         self.n_heads = n_heads
-#
-#         # TODO: create/initialize any necessary parameters or layers
-#         # Initialize all weights and biases uniformly in the range [-k, k],
-#         # where k is the square root of 1/n_units.
-#         # Note: the only Pytorch modules you are allowed to use are nn.Linear
-#         # and nn.Dropout
-#         self.query_linear = nn.Linear(n_units, n_units)
-#         self.key_linear = nn.Linear(n_units, n_units)
-#         self.value_linear = nn.Linear(n_units, n_units)
-#         self.dropout = nn.Dropout(dropout)
-#         self.output = nn.Linear(n_units, n_units)
-#
-#     def forward(self, query, key, value, mask=None):
-#         # TODO: implement the masked multi-head attention.
-#         # query, key, and value all have size: (batch_size, seq_len, self.n_units)
-#         # mask has size: (batch_size, seq_len, seq_len)
-#         # As described in the .tex, apply input masking to the softmax
-#         # generating the "attention values" (i.e. A_i in the .tex)
-#         # Also apply dropout to the attention values.
-#         batch_size = query.size(0)
-#
-#         query = self.query_linear(query).view(batch_size, -1, self.n_heads, self.d_k).transpose(1,2)
-#         key = self.key_linear(key).view(batch_size, -1, self.n_heads, self.d_k).transpose(1,2)
-#         value = self.value_linear(value).view(batch_size, -1, self.n_heads, self.d_k).transpose(1,2)
-#
-#         # generate "attention values" (apply dropout if present)
-#         A = self.attention(query, key, value, self.d_k, batch_size, mask, self.dropout)
-#
-#         output = self.output(A)
-#
-#         return output # size: (batch_size, seq_len, self.n_units)
-#
-#     def attention(self, q, k, v, d_k, batch_size, mask=None, dropout=None):
-#
-#         a_i_term = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
-#
-#         if mask is not None:
-#             mask = mask.unsqueeze(1)
-#             #scores = torch.mul(scores, mask) - 10e9 * (1 - mask)
-#             a_i_term = a_i_term.masked_fill(mask == 0, -10e9)
-#         attention_values = F.softmax(a_i_term, dim=-1)
-#
-#         if dropout is not None:
-#             attention_values = dropout(attention_values)
-#
-#         h_i = torch.matmul(attention_values, v)
-#
-#         # concatenate attention heads
-#         A = h_i.transpose(1,2).contiguous().view(batch_size, -1, self.n_units)
-#
-#         return A
-
 
 #----------------------------------------------------------------------------------
 # The encodings of elements of the input sequence